4ad361607165e00a9d9bfd4f07180cd1c4aae594
Abir Dutta <abirdutta390@gmail.com>
2025-08-26 23:13:54 +0530

    start0

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Evermore</h1>
+      <p className="text-center text-sm">Create your event, let others share there moments</p>
+    </div>
+  );
+}
